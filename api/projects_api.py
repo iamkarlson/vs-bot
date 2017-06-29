e5c9cf8f17a4438d779e4aed082540e231c908ce
Georgy Grigoryev <grigoryev@ntpayments.com>
2017-06-29 13:29:49 +0400

    porjects api stub

---
+++ api/projects_api.py
@@ -0,0 +1,26 @@
+import pprint
+
+from api.baseapi import BaseApi
+
+class ProjectsApi(BaseApi):
+    def __init__(self):
+        BaseApi.__init__(self,"projects")
+        self.build_client()
+        print('projects api ctor.')
+
+    def GET(self):
+        self.build_client()
+        projects= self.get()
+        return projects
+
+    def GET(self,id):
+        self.build_client()
+        project = self.get(id)
+        return project
+
+
+if __name__=="__main__":
+    api = ProjectsApi()
+    print(api.api_url())
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(api.get())
