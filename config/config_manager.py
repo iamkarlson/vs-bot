47e5bb508a1637f4b3f15beea642b3a6127131aa
Georgy Grigoryev <iamkarlson@gmail.com>
2017-10-07 21:03:30 +0400

    add settings from environment

--- config/config_manager.py
+++ config/config_manager.py
@@ -3,15 +3,22 @@ import configparser, os
 
 class config_man:
     def __init__(self, config_path):
-        config = configparser.ConfigParser()
-        config.read(config_path)
-        section = config['DEFAULT']
-        self.TOKEN = section['token']
-        self.URL = section['url']
-        self.COLLECTION = section['collection']
-        section = config['BOT']
-        self.BOT_ID = section['bot_id']
-        self.SLACK_BOT_TOKEN = section['slack_bot_token']
+        if config_path:
+            config = configparser.ConfigParser()
+            config.read(config_path)
+            section = config['DEFAULT']
+            self.TOKEN = section['token']
+            self.URL = section['url']
+            self.COLLECTION = section['collection']
+            section = config['BOT']
+            self.BOT_ID = section['bot_id']
+            self.SLACK_BOT_TOKEN = section['slack_bot_token']
+        else:
+            self.TOKEN = os.environ.get('VSTS_TOKEN')
+            self.URL = os.environ.get('VSTS_URL')
+            self.COLLECTION = os.getenv('VSTS_COLLECTION',"DefaultCollection")
+            self.BOT_ID = os.environ.get('SLACK_BOT_ID')
+            self.SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
 
 
 if __name__ == '__main__':
